0c2f295ffa9d2cbeb436fb1dbab22bb0a4755dec
Chris Breaux <christopherjbreaux@gmail.com>
2019-05-05 21:16:55 -0700

    add routing for details page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Dashboard from './components/dashboard/Dashboard';
 import Navbar from './components/nav/Navbar';
+import EntryDetails from './components/entries/EntryDetails';
+
 
 class App extends Component {
   render() {
@@ -11,7 +13,10 @@ class App extends Component {
       <BrowserRouter>
         <div className="test">
           <Navbar />
-          <Dashboard />
+          <Switch>
+            <Route exact path='/' component={Dashboard} />
+            <Route path='/entry/:id' component={EntryDetails} />
+          </Switch>
         </div>
       </BrowserRouter>
     );
---
+++ src/components/entries/EntryDetails.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function EntryDetails() {
+  return (
+    <div>
+      <h1>Entry Name</h1>
+      <p>The date</p>
+      <p>Mushroom - Qty</p>
+      <p>Mushroom - Qty</p>
+      <p>Mushroom - Qty</p>
+      <p> Graph goes here? </p>
+    </div>
+  );
+}
+
+export default EntryDetails;
