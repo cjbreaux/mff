d59ab3857e6d7e8678aaef3ba41ec33e77df5beb
Chris Breaux <christopherjbreaux@gmail.com>
2019-05-10 10:53:14 -0700

    capture user input on SignIn component

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,19 +1,32 @@
 import React from 'react';
-import styles from './styles.scss';
+import './styles.scss';
 
 function SignIn() {
+  let _email;
+  let _password;
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log('submitted')
+    console.log(_email.value, _password.value)
+  }
   return(
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h5> Sign In </h5>
         <div className='flex'>
           <div>
             <label htmlFor='email'>Email</label>
-            <input type='email' />
+            <input
+              type='email'
+              id='email'
+              ref={(input)=> {_email = input;}} />
           </div>
           <div>
             <label htmlFor='password'>Password</label>
-            <input type='password' />
+            <input
+              type='password'
+              id='password'
+              ref={(input)=> {_password = input;}} />
           </div>
           <div>
             <button type='submit'>Login</button>
