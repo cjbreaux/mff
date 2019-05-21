76baaf860d5c70a4ab8b608b793654bb25b2efca
Chris Breaux <christopherjbreaux@gmail.com>
2019-05-21 11:21:02 -0700

    add and remove dynamic markers

--- src/components/maps/TestMap.js
+++ src/components/maps/TestMap.js
@@ -8,36 +8,56 @@ import { connect } from 'react-redux';
 
 
 class TestMap extends Component {
-  state = {
-    // lat: 45.520,
-    // lng: -122.677,
-    zoom: 13,
+  constructor(props) {
+    super(props);
+    if (this.props.lat && this.props.lng) {
+      this.setState({markers: [this.props.late, this.props.long]})
+    }
+    this.state = {
+      // lat: 45.520,
+      // lng: -122.677,
+      markers: [],
+      zoom: 13,
+    }
+  }
+
+  addMarker = (e) => {
+    const {markers} = this.state
+    markers.push(e.latlng)
+    this.setState({markers})
+  }
+
+  removeMarker = index => () => {
+    this.setState({
+      markers: this.state.markers.filter((loc, pos) => index !== pos)
+    });
   }
 
   render() {
-    console.log(typeof this.props.lat)
-    // const position = [this.state.lat, this.state.lng]
     if (this.props.lat && this.props.lng) {
       const position = [this.props.lat, this.props.lng]
       return (
         <Map
           center={position}
           style={{ width: '100%', height: '600px' }}
+          onClick={this.addMarker}
           zoom={this.state.zoom}>
           <TileLayer
             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-          <Marker position={position}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
+          {this.state.markers.map((position, index) =>
+            <Marker
+              key={`marker-${index}`}
+              onClick={this.removeMarker(index)}
+              position={position}>
+            </Marker>
+          )}
         </Map>
       )
     } else {
       return (
-        <p>...loading</p>
+        <p>...loading map</p>
       )
     }
   }
