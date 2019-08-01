dc45d2383f475a678447d16b6df4f36693f0e9af
Chris Breaux <christopherjbreaux@gmail.com>
2019-07-31 19:33:22 -0700

    add smooth scroll to entries

--- src/components/entries/EntrySummary.js
+++ src/components/entries/EntrySummary.js
@@ -5,9 +5,17 @@ import setSelectedEntry from '../../actions/entryActions';
 
 function EntrySummary({entry,setSelectedEntry}) {
 
+  function topFunction() {
+    const c = document.documentElement.scrollTop || document.body.scrollTop;
+    if (c > 0) {
+      window.requestAnimationFrame(topFunction);
+      window.scrollTo(0, c - c / 8);
+    }
+}
+
   return(
     <div
-      className='bg-lightest-blue ba bw1 b--blue br4 grow shadow-5 mt2 pl3' onClick={()=>setSelectedEntry(entry.entryId)}>
+      className='bg-lightest-blue ba bw1 b--blue br4 grow shadow-5 mt2 pl3' onClick={()=>{setSelectedEntry(entry.entryId); topFunction()}}>
       <h2 className='f3'>{entry.entryName}</h2>
       <p>{entry.notes}</p>
       {entry.createdAt ? <p className='i'>{moment(entry.createdAt.toDate()).calendar()}</p> : null }
